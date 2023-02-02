94e8e90a8eca5f65087fbb5d97738518671dd09a
Fatih ÇELİK <fatihcelikofficialtr@gmail.com>
2023-02-03 02:59:39 +0300

    +

--- sayfa/fctest/liste.js
+++ sayfa/fctest/liste.js
@@ -1,62 +1,19 @@
-var
-  filters = {
-    user: null,
-    status: null,
-    milestone: null,
-    priority: null,
-    minerbonus: null
-  };
-
-function updateFilters() {
-  $('.task-list-row').hide().filter(function() {
-    var
-      self = $(this),
-      result = true; // not guilty until proven guilty
-    
-    Object.keys(filters).forEach(function (filter) {
-      if (filters[filter] && (filters[filter] != 'Hepsi') && (filters[filter] != 'Hepsi')) {
-        result = result && filters[filter] === self.data(filter);
+window.onload = loader;
+    function loader() {
+      let loaderPage = document.querySelector('.loader-page');
+      let counter = document.querySelector('.loader-counter');
+      let fill = document.querySelector('.loader-fill');
+      let amount = 10;
+      let interval = setInterval(loop, 50);
+      function loop() {
+        if (amount >= 100) {
+          clearInterval(interval);
+          loaderPage.style.visibility = 'hidden';
+          loaderPage.style.opacity = '0'
+        }else {
+          amount++;
+          fill.style.width = amount + '%';
+          counter.textContent = '%' + amount * 1;
+        }
       }
-    });
-
-    return result;
-  }).show();
-}
-
-function changeFilter(filterName) {
-  filters[filterName] = this.value;
-  updateFilters();
-}
-
-// Assigned User Dropdown Filter
-$('#assigned-user-filter').on('change', function() {
-  changeFilter.call(this, 'user');
-});
-
-// Task Status Dropdown Filter
-$('#status-filter').on('change', function() {
-  changeFilter.call(this, 'status');
-});
-
-// Task Milestone Dropdown Filter
-$('#milestone-filter').on('change', function() {
-  changeFilter.call(this, 'milestone');
-});
-
-// Task Priority Dropdown Filter
-$('#priority-filter').on('change', function() {
-  changeFilter.call(this, 'priority');
-});
-
-// Task Tags Dropdown Filter
-$('#minerbonus-filter').on('change', function() {
-  changeFilter.call(this, 'minerbonus');
-});
-
-/*
-future use for a text input filter
-$('#search').on('click', function() {
-    $('.box').hide().filter(function() {
-        return $(this).data('order-number') == $('#search-criteria').val().trim();
-    }).show();
-});*/
+    }
