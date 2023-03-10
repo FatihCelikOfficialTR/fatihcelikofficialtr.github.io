54303a3f45079042eee72e942567ef465a258479
Fatih ÇELİK <fatihcelikofficialtr@gmail.com>
2023-03-10 18:20:24 +0300

    Update haftalikodul.js

--- js/haftalikodul.js
+++ js/haftalikodul.js
@@ -1,5 +1,5 @@
         var typed2 = new Typed('#haftalıködüller', {
-            strings: ["BTC: 37500 SAT", "ETH: 0.0065","BNB: 0.038","MATIC: 5","DOGE: 120","SOL: 0.19"], // ☛ Güncellemeyi bekleyiniz.. ☚
+            strings: ["BTC: 40000 SAT", "ETH: 0.007","BNB: 0.044","MATIC: 5.5","DOGE: 140","SOL: 0.23"], // ☛ Güncellemeyi bekleyiniz.. ☚
             typeSpeed: 70,
             backSpeed: 30,
             backDelay: 10000,
