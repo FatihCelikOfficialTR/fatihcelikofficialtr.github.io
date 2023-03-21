12025fe11d755f477a1e41ac57ea8b62209be5a6
Fatih ÇELİK <fatihcelikofficialtr@gmail.com>
2023-03-21 03:58:11 +0300

    +

--- js/fatihcelik.js
+++ js/fatihcelik.js
@@ -1,22 +1,3 @@
-/* DARK MOD AYARI */
-const options = {
-  bottom: '64px', // default: '32px'
-  right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#f7f9fb',  // default: '#fff'
-  buttonColorDark: '#100f2c',  // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: true, // default: true,
-  label: '🌓', // default: ''
-  autoMatchOsTheme: true // default: true
-}
-
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
-
-
 /*MENÜ AYARLARI İÇİN KODLAR*/
 "use strict";
 
