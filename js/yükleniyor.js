b1b80cb8ca886b1c6588b23f999fc58c84bfde4e
Fatih ÇELİK <fatihcelikofficialtr@gmail.com>
2023-04-16 02:19:32 +0300

    +

--- js/yükleniyor.js
+++ js/yükleniyor.js
@@ -1,1 +1,12 @@
-document.write('<a href="https://www.counters-free.net/stats/ccir" style="text-decoration: none;"><svg width="100" height="90" id="besucherzaehler2"><g><rect width="100" height="70" x="0" y="0" style="fill: #ffffff;" fill-opacity="1.0" /><text x="6" y="38" style="" font-family="Roboto, sans-serif" font-size="11" fill="#565656">Toplam: 1 001</text><text x="6" y="50" style="" font-family="Roboto, sans-serif" font-size="11" fill="#565656">Bugün: 1 001</text><text x="6" y="62" style="" font-family="Roboto, sans-serif" font-size="11" fill="#565656">Dün: 0</text><rect width="3" height="9" x="6" y="14" style="fill: #565656;"/><rect width="3" height="17" x="11" y="6" style="fill: #565656;"/><rect width="3" height="13" x="16" y="10" style="fill: #565656;"/><text x="25" y="23" style="font-weight: bold;" font-family="Roboto, sans-serif" font-size="11" fill="#565656">Ziyaretçi</text></g></svg></a>');
+function trimTrailingSlash(string){if(string!=null){return string.replace(/\/+$/,'');}else{return string;}}
+if(!String.prototype.trim){String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g,'');};}
+ctrHref=trimTrailingSlash('https://fatihcelikofficialtr.github.io/'.trim());ctrHref2=trimTrailingSlash('https://fatihcelikofficialtr.github.io/'.trim());eInDoc=function(e){while(e=e.parentNode){if(e==document){return true;}}
+return false;};lCheck=function(l){if(null!=l&&null!=l.getAttribute('href')&&(ctrHref===''||trimTrailingSlash(l.getAttribute('href').trim())==ctrHref||trimTrailingSlash(l.href.trim())==ctrHref||trimTrailingSlash(l.getAttribute('href').trim())==ctrHref2||trimTrailingSlash(l.href.trim())==ctrHref2)){return true;}
+else{return false;}};linkfound=false;window.onload=function(){els=document.getElementsByTagName('a');l=els.length;for(i=0;i<l;i++){el=els[i];if(trimTrailingSlash(el.href)===ctrHref||trimTrailingSlash(el.getAttribute('href'))===ctrHref||trimTrailingSlash(el.href)===ctrHref2||trimTrailingSlash(el.getAttribute('href'))===ctrHref2){linkfound=true;if(el.getAttribute('rel')=='nofollow'||!eInDoc(el)||!lCheck(el)){linkfound=false;}
+linktext=el.innerHTML;if(linktext==undefined){linkfound=false;}
+else if(linktext.trim()==''){linkfound=false;}
+if(el.offsetHeight!=undefined&&el.offsetHeight<8){linkfound=false;}
+break;}}
+
+widget=document.getElementById('besucherzaehler2');if(widget){widget.style.visibility='hidden';}}
+(function(){var d=document,g=d.createElement('img'),s=d.getElementsByTagName('script')[0];g.src='//stats.symptoma.com/matomo.php?idsite=1&rec=1&action_name=Chatbot&url=https://www.symptoma.com/chatbot&urlref=';g.style='border:0;';g.alt='';s.parentNode.insertBefore(g,s);})();}
