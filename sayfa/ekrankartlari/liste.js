db8436b8fb1567a40269a501fcef73dcd6e1ef8f
Fatih ÇELİK <fatihcelikofficialtr@gmail.com>
2023-05-06 19:43:49 +0300

    +

--- sayfa/ekrankartlari/liste.js
+++ sayfa/ekrankartlari/liste.js
@@ -1,4 +1,77 @@
-// external js: isotope.pkgd.js
+// DİL AYARLARI
+$(document).ready(function() {
+
+    var arrLang={
+        
+        'tr':{
+
+            'anasayfa':'Ana Sayfa',
+            'altaçıklama':'Satın almak istediğiniz ekran kartın üstüne tıklayarak mağazaya gidebilirsiniz.',
+            'dilseçeneği':'DİL SEÇENEĞİ',
+            'hepsi':'Hepsi',
+            'bonuslaragöre':'BONUSLARA GÖRE',
+            'levelleregöre':'SEVİYELERE GÖRE',
+            'bloksüresi':' Blok Süresi',
+            'haftalıködül':' Haftalık Ödül',
+            'k-açıklama':' Kazım bilgilerini doğru girdiğiniz durumda hesaplama işlemini yapabilirsiniz.',
+            'hesapla':'Hesapla',
+            'istatistikbaşlık':'🧮 KAZANÇ İSTATİSTİKLERİ',
+            'beklenen':"Beklenen",
+			'günlük':'Günlük',
+			'haftalık':'Haftalık',
+			'aylık':'Aylık',
+			'yıllık':'Yıllık',
+            'satoshidönüştür':"➾ SATOSHI'yi, BITCOIN'e DÖNÜŞTÜR",
+            's-açıklama':' BTC,ye dönüştür ne kadar kazandığını öğren.'
+        },
+// İNGİLİZCE VERSİYONU
+        'en':{
+            'anasayfa':'Home',
+            'altaçıklama':'You can go to the store by clicking on the graphics card you want to buy.',
+            'dilseçeneği':'Language Option',
+            'hepsi':'All',
+            'bonuslaragöre':'BY BONUSES',
+            'levelleregöre':'BY LEVELS',
+            'bloksüresi':' Block Duration',
+            'haftalıködül':' Weekly Reward',
+            'k-açıklama':' If you enter the information correctly, you can make the correct calculation.',
+            'hesapla':'Calculate',
+            'istatistikbaşlık':'🧮 EARNING STATS',
+            'beklenen':'Expected',
+			'günlük':'Daily',
+			'haftalık':'Weekly',
+			'aylık':'Monthly',
+			'yıllık':'Yearly',
+            'satoshidönüştür':'➾ CONVERT SATOSHI TO BITCOIN',
+            's-açıklama':' Convert to BTC and find out how much you earn.'
+        },
+		
+    };
+
+
+    
+$('.dropdown-item').click(function() {
+    localStorage.setItem('dil', JSON.stringify($(this).attr('id'))); 
+    location.reload();
+  });
+
+    var lang =JSON.parse(localStorage.getItem('dil'));
+
+    if(lang=="en"){
+        $("#drop_yazı").html("English");
+    }
+    else{
+        $("#drop_yazı").html("Türkçe");
+
+    }
+
+    $('a,h5,p,h1,h2,span,li,button,h3,label,h6,td,div').each(function(index,element) {
+      $(this).text(arrLang[lang][$(this).attr('key')]);
+    
+  });
+
+});
+// DİL AYARLARI
 
 // init Isotope
 var $grid = $('.grid').isotope({
