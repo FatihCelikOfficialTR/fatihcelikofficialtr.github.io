071dfd9ede40de04477ec86084f333f07537100e
Fatih ÇELİK <fatihcelikofficialtr@gmail.com>
2023-09-02 02:03:57 +0300

    +

--- js/adblock.js
+++ js/adblock.js
@@ -1,187 +1,86 @@
-/**
- * Adi.js
- */
+function startAdAlertDisplayProcess() {
+    //document.cookie = "adBlockAlertShown=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    getCookieValue("adBlockAlertShown").then(
+        result => {
+            console.log("ad Block Alert Already Shown");
+        },
+        function (err) {
+            console.log("adBlockAlertNotShown");
+            $("#adBlockAlertDiv").modal("show");
 
-;(function($) {
+            $("#adBlockAlertDiv").on("hidden.bs.modal", function (e) {
+                //set cookie
+                var dat = new Date();
+                dat.setDate(dat.getDate() + 1);
+                document.cookie =
+                    "adBlockAlertShown=true; expires=" + dat.toUTCString() + "; path=/";
+                console.log("adalert cookie set");
+            });
+        }
+    );
+}
 
-  'use strict';
-
-  var Adi;
-
-  $.adi = function(args) {
-
-    /**
-     * Merge defaults with user options
-     */
-
-    var options = $.extend({}, Adi.defaults, args);
-    return new Adi(options);
-  };
-
-  /**
-   * Constructor
-   */
-
-  Adi = function(args) {
-
-    /**
-     * Merge this with user options
-     */
-
-    $.extend(this, args);
-
-    if (this._check()) {
-      this._init();
-      this.active();
-    }
-
-    if (!this._check()) {
-      this.inactive();
-    }
-  };
-
-  /**
-   * Check for $.adblock
-   */
-
-  Adi.prototype._check = function() {
-    return $.adblock === undefined
-  };
-
-  /**
-   * Start plugin	 
-   */
-
-  Adi.prototype._init = function() {
-    this._append();
-  };
-
-  /**
-   * Set template
-   */
-
-  Adi.prototype._setTemplate = function(title, content) {
-
-    return '<div class="jquery-adi">' +
-      '<div class="jquery-adi_content">' +
-      '<button class="jquery-adi_close"></button>' +
-      '<h2>' + title + '</h2>' +
-      '<p>' + content + '</p>' +
-      '</div>' +
-      '</div>';
-  };
-
-  /**
-   * Append html
-   */
-
-  Adi.prototype._append = function(callback) {
-
-    this.$el = $(this._setTemplate(this.title, this.content)).appendTo($(document.body)).addClass(this.theme);
-    this._show();
-  };
-
-  /**
-   * Show modal
-   */
-
-  Adi.prototype._show = function() {
-
-    var that = this;
-
-    this.$el.show();
-    this._center();
-    this._controls();
-
-    this.onOpen(this.$el);
-  };
-
-  /**
-   * Modal controls
-   */
-
-  Adi.prototype._controls = function() {
-
-    var that = this;
-
-    function close() {
-      that.$el.hide();
-      that.onClose(that.$el);
-    }
-
-    this.$el.on('click', '.jquery-adi_close', close);
-    $(document).on('keyup', function(e) {
-      if (e.keyCode == 27)
-        close();
+function getCookieValue(key) {
+    return new Promise(function (resolve, reject) {
+        var itemFound = false;
+        document.cookie.split(";").map(item => {
+            if (item.indexOf(key) >= 0) {
+                itemFound = true;
+                resolve(item.split("=")[1]);
+            }
+        });
+        if (itemFound == false) {
+            reject(Error("No Data"));
+        }
     });
-  };
+}
 
-  /**
-   * Center modal
-   */
-
-  Adi.prototype._center = function() {
-    var $modal = this.$el.find('.jquery-adi_content');
-    $modal.css('margin-top', -Math.abs($modal.outerHeight() / 2));
-  };
-
-  /**
-   * Defaults
-   */
-
-  Adi.defaults = {
-    title: 'Adblock detected!',
-    content: 'We noticed that you may have an Ad Blocker turned on. Please be aware that our site is best experienced with Ad Blockers turned off.',
-    theme: 'light',
-    onOpen: function() {},
-    onClose: function() {},
-    active: function() {},
-    inactive: function() {}
-  };
-
-})(jQuery);
-
-/*==========================
-      End plugin
-===========================*/
-
-$(document).ready(function() {
-
-  $.adi({
-    theme: 'dark',
-    onOpen: function(el) {
-      /* $.adi working with animate.css */
-      el.find('.jquery-adi_content').addClass('animated bounceInDown')
-    },
-    onClose: function(el) {
-      /**
-       * Redirect
-       * ========
-       * window.location.href('http://some-website')
-       * 
-       * Dont let user to see content / reload page
-       * ==========================================
-       * window.location.reload(true);
-       */
-    },
-    inactive: function() {
-
-      var tpl = '<h3>You cool, G.</h3>' +
-        '<img src="//media.giphy.com/media/POWvddaQEHrgc/giphy.gif" />';
-
-      $('.container').append(tpl);
-
-      console.log('Adblock not detected :)');
-    },
-    active: function() {
-
-      var tpl = '<h3>You not cool, G</h3>' +
-        '<img src="//media.giphy.com/media/4lhJQOACaIfWU/giphy.gif" />';
-
-      $('.container').append(tpl);
-
-      console.log('Adblock detected :(')
+window.setTimeout(function () {
+    if (window.canRunAds === undefined) {
+        var node = document.createElement("div");  
+        document.body.appendChild(node);
+        node.innerHTML =adBlockHtml;
+        startAdAlertDisplayProcess();
     }
-  });
+}, 1000);
 
-});
+var adBlockHtml=`<div class="modal fade" id="adBlockAlertDiv">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header" style="background-color: #1955a5;color:white;">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <div style="border-bottom: 2px solid #174e98;padding:10px 0;">CoreVertical.com</div>
+        <h2 class="modal-title">We noticed you’re blocking ads.</h2>
+      </div>
+      <div class="modal-body">
+        <p> Our website is made possible by displaying online advertisements to our visitors.<br> Please consider supporting us by disabling your ad blocker on our website.</p>
+        
+        <p>Here are the steps to whitelist this site</p>
+        <ul>
+          <li>
+            <b>AdBlock Plus</b>
+Click ”Enabled on this site”  or  ”Disable on corevertical.com.” Refresh the page.
+          </li>
+          <li>
+            <b>AdBlock</b>
+Click on "AdBlock" sign on browser. Select "Don't run on pages of this site".
+          </li>
+          <li>
+            <b> Ghostery</b>
+Version 6.0: click ”trust site.” Refresh the page. Older versions:  click “whitelist site.”  You will see the message ”site is whitelisted.” Click “reload the page to see your changes.”
+          </li>  
+          <li>
+            <b>uBlock</b>
+Click the big blue ”Power” icon. Refresh the page.
+          </li>
+        </ul>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal" >Continue to site</button>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->`
